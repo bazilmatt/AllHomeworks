2ff6f63ef12c0ae722fb2fcd8dc5bffc47cb3c6b
Vasiliy Matveev <bazilmat@yandex.ru>
2020-05-31 08:50:05 +0300

    HomeWork 9 - it's alive! Я удивлен, но работает.

---
+++ src/main/java/Homework9/Task1/RuntimeCompilation.java
@@ -0,0 +1,64 @@
+package Homework9.Task1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Scanner;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+public class RuntimeCompilation {
+
+    public static void main(String[] args) throws Exception {
+
+        // Create an empty source File
+        File sourceFile = new File("Hello.java");
+        String filePath = sourceFile.getPath();
+
+        // hardcode for class, and get classname
+        String classname = sourceFile.getName().split("\\.")[0];
+        String hardCode = "public class Hello { \n public void hello() {\n ";
+
+        //buffer code
+        Scanner in = new Scanner(System.in);
+        boolean flag = true;
+        StringBuilder sourceCode = new StringBuilder();
+        String buffer;
+        sourceCode.append('\n');
+
+        //
+        while (flag){
+            buffer = in.nextLine();
+            if (buffer.equals("quit")){
+                flag = false;
+                continue;
+            }
+            sourceCode.append(buffer);
+            sourceCode.append('\n');
+        }
+
+        // write the source code into the source file
+        FileWriter writer = new FileWriter(sourceFile);
+        writer.write(hardCode);
+        writer.append(sourceCode);
+        writer.append("} }");
+        writer.close();
+
+        // compile
+        File parentDirectory = sourceFile.getParentFile();
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        File classFileDir = new File("E:\\Kraken\\Allhomeworks\\");
+        compiler.run(null, null, null, sourceFile.getPath());
+        // load
+        System.out.println(classFileDir);
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{ classFileDir.toURI().toURL()});
+        Class<?> Hello = classLoader.loadClass("Hello");
+        Method test = Hello.getDeclaredMethod("hello");
+        test.invoke(Hello.newInstance());
+
+    }
+}
+
