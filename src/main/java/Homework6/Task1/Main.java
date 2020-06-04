bbcc03f127869087761c16a7e97f96065d47939f
Vasiliy Matveev <bazilmat@yandex.ru>
2020-06-04 11:07:25 +0300

    HomeWork 6 - перезалив в новый репозиторий + исправления

---
+++ src/main/java/Homework6/Task1/Main.java
@@ -0,0 +1,42 @@
+package Homework6.Task1;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * Homework 6 Task 1
+ * Logic and main structure of program
+ *
+ * Program must read the file and put all words in collection without repeats
+ *
+ * @author Matveev
+ *
+ */
+
+public class Main {
+    static  Set<String> strings = new TreeSet<>();
+    static String s = "";
+
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("Example")))){
+            while ((s = reader.readLine()) != null ) {
+                StringTokenizer st = new StringTokenizer(s, " ");
+                while (st.hasMoreTokens()){
+                    strings.add(st.nextToken());
+                }
+            }
+        }
+
+        //strings.addAll();
+        List<String> listOfStrings = new ArrayList<>();
+        listOfStrings.addAll(strings);
+        listOfStrings.sort(String::compareToIgnoreCase);
+        System.out.println(listOfStrings.toString());
+    }
+
+
+}
---
+++ src/main/java/Homework6/task2/Main.java
@@ -0,0 +1,36 @@
+package Homework6.task2;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Homework 6 Task 2
+ * Logic and main structure of program
+ * <p>
+ * Program must write something into the file...
+ * /*
+ *
+ * @author Matveev
+ */
+
+public class Main {
+
+    static String s = "";
+
+    public static void main(String[] args) throws IOException {
+        PhraseGenerator pg = new PhraseGenerator();
+        //FileWriter fw = new FileWriter("1");
+
+        for (int i = 0; i < 5; i++) {
+            OutputStreamWriter fos = new OutputStreamWriter(new FileOutputStream(String.valueOf(i)));
+            try {
+                fos.append(pg.generateParagraph());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                fos.close();
+            }
+        }
+
+    }
+}
