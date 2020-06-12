4a47c0e7e7f63a3c1b35d8f2330bda5d04474f8f
Vasiliy Matveev <bazilmat@yandex.ru>
2020-06-12 23:39:38 +0300

    homework12 - meta test

--- src/main/java/Homework12/task2/MemoryMetaError.java
+++ src/main/java/Homework12/task2/MemoryMetaError.java
@@ -1,5 +1,46 @@
 package Homework12.task2;
 
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Scanner;
+
 public class MemoryMetaError {
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+
+        // hardcode for class
+        for (int i = 1; i <10; i++){
+
+            File sourceFile = new File(i +"class"+ ".java");
+            String hardCode = "public class "+ i +"class "+ " { \n public void doWork() {\n String[] a" + i +" = new String[120]; +} }";
+
+            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+            File classFileDir = new File("E:"+File.separator+"Kraken"+File.separator+"Allhomeworks\\");
+            compiler.run(null, null, null, sourceFile.getPath());
+
+            FileWriter fr = new FileWriter(sourceFile);
+            fr.append(hardCode);
+            fr.write(hardCode);
+            fr.close();
+            System.out.println(classFileDir);
+            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{ classFileDir.toURI().toURL()});
+            Class<?> Worker = classLoader.loadClass(i+"class");
+        }
+
+        // compile
+
+        // load
+
+
+    }
+
 }
+
