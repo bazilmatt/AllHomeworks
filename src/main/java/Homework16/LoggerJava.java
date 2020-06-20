fcd8f6d9eb20ae71c4a576ff0bdbc3cc65b4533d
Vasiliy Matveev <bazilmat@yandex.ru>
2020-06-20 23:25:18 +0300

    homework16 - logger start

---
+++ src/main/java/Homework16/LoggerJava.java
@@ -0,0 +1,14 @@
+package Homework16;
+
+
+import org.apache.logging.log4j.*;
+
+
+public class LoggerJava {
+    private static Logger logger = LogManager.getLogger(LoggerJava.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
